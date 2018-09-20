9d1214e15963bf9ca9904015682ec891e35f7771
Miklos Vajna <vmiklos@vmiklos.hu>
2018-09-20 23:28:46 +0200

    Fix Release vs Debug mix on Windows
    Just focus on Release for now

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,6 +5,7 @@ REM found in the LICENSE file.
 cmake -E remove_directory workdir
 md workdir
 cd workdir
-cmake -DCMAKE_BUILD_TYPE=Debug -DODFSIG_INTERNAL_LIBZIP=ON -DODFSIG_INTERNAL_LIBGTEST=ON -DODFSIG_INTERNAL_LIBXML2=ON -DODFSIG_INTERNAL_XMLSEC=ON -DODFSIG_INTERNAL_ZLIB=ON ..
-cmake --build .
-cmake --build . --target check
+cmake -DODFSIG_INTERNAL_LIBZIP=ON -DODFSIG_INTERNAL_LIBGTEST=ON -DODFSIG_INTERNAL_LIBXML2=ON -DODFSIG_INTERNAL_XMLSEC=ON -DODFSIG_INTERNAL_ZLIB=ON ..
+cmake --build . --config Release
+REM disable due to the use-after-free bug in xmlsec 1.2.26's mscng backend
+REM cmake --build . --config Release --target check
