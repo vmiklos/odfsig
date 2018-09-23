557955269ad33adda33df95a0d88da0a62cf5ca7
Miklos Vajna <vmiklos@vmiklos.hu>
2018-09-23 08:54:06 +0200

    Default to internal libs on Windows

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,7 +5,7 @@ REM found in the LICENSE file.
 cmake -E remove_directory workdir
 md workdir
 cd workdir
-cmake -DODFSIG_INTERNAL_LIBS=ON ..
+cmake ..
 cmake --build . --config Release
 REM disable due to the use-after-free bug in xmlsec 1.2.26's mscng backend
 REM cmake --build . --config Release --target check
