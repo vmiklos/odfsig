6b818c4550e5c71ec044c519f1bf3ff3dd20b237
Miklos Vajna <vmiklos@vmiklos.hu>
2018-10-21 13:54:02 +0200

    Windows: enable similar amount of warnings than on Linux

--- cmake/HandleOdfsigOptions.cmake
+++ cmake/HandleOdfsigOptions.cmake
@@ -11,6 +11,8 @@ endif ()
 
 set(CMAKE_CXX_STANDARD 14)
 if (WIN32)
+    # more or less equivalent of the below -Wall and -Wextra
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
     # would warn on e.g. getenv()
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_CRT_SECURE_NO_DEPRECATE")
     # possible loss of data, libzip vs libxml2 different int types
