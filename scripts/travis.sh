5898d1aad60b2bfc620c3f655cec8b5f974cb8ac
Miklos Vajna <vmiklos@vmiklos.hu>
2018-11-01 15:33:46 +0100

    macOS: add CI

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,8 +3,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Baseline: Ubuntu Trusty 14.04.
+# Baseline: Ubuntu Trusty 14.04 and macOS 10.12.
 
-CC=gcc-7 CXX=g++-7 scripts/build.sh "$@"
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    export CC=gcc-7
+    export CXX=g++-7
+fi
+
+scripts/build.sh "$@"
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
