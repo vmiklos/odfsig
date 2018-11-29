0e30c2c0acce109f7f1506176657ccbf7c4a1612
Miklos Vajna <vmiklos@vmiklos.hu>
2018-11-29 22:41:23 +0100

    scripts: sanitizers -> asan-ubsan
    In preparation of a future possible msan type.

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,7 +26,7 @@ do
         --iwyu)
             cmake_args+=" -ODFSIG_IWYU=ON"
             ;;
-        --sanitizers)
+        --asan-ubsan)
 	    export ASAN_OPTIONS=handle_ioctl=1:detect_leaks=1:allow_user_segv_handler=1:use_sigaltstack=0:detect_deadlocks=1:intercept_tls_get_addr=1:check_initialization_order=1:detect_stack_use_after_return=1:strict_init_order=1:detect_invalid_pointer_pairs=1
 	    export LSAN_OPTIONS=print_suppressions=0:report_objects=1
 	    export UBSAN_OPTIONS=print_stacktrace=1
