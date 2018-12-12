b364e06e22acf510bf366d82b1ba7f90871df58a
Miklos Vajna <vmiklos@vmiklos.hu>
2018-12-12 13:03:00 +0100

    Improve the fuzzer a bit
    Disable certificate validation, assuming nss cert validation has been
    fuzzed already -- and also never return errors, as the caller asserts
    success.

--- apps/fuzz.cxx
+++ apps/fuzz.cxx
@@ -10,14 +10,15 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     std::unique_ptr<odfsig::Verifier> verifier(
         odfsig::Verifier::create(std::string()));
+    verifier->setInsecure(true);
     if (!verifier->openZipMemory(data, size))
     {
-        return 1;
+        return 0;
     }
 
     if (!verifier->parseSignatures())
     {
-        return 1;
+        return 0;
     }
 
     for (const auto& signature : verifier->getSignatures())
