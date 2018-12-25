34a1edaed922391c4234c4537b17fd2c08f05cd0
Miklos Vajna <vmiklos@vmiklos.hu>
2018-12-25 12:25:45 +0100

    iwyu: fix typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,7 +25,7 @@ do
             export CCACHE_CPP2=1
             ;;
         --iwyu)
-            cmake_args+=" -ODFSIG_IWYU=ON"
+            cmake_args+=" -DODFSIG_IWYU=ON"
             ;;
         --asan-ubsan)
 	    export ASAN_OPTIONS=handle_ioctl=1:detect_leaks=1:allow_user_segv_handler=1:use_sigaltstack=0:detect_deadlocks=1:intercept_tls_get_addr=1:check_initialization_order=1:detect_stack_use_after_return=1:strict_init_order=1:detect_invalid_pointer_pairs=1
