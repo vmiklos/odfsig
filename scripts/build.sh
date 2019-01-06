9e354faba36cfe69844acaa074642260923d7024
Miklos Vajna <vmiklos@vmiklos.hu>
2019-01-06 14:22:28 +0100

    asan-ubsan: simplify options
    Just request the two default sanitizers + opt in for one useful feature.

--- scripts/build.sh
+++ scripts/build.sh
@@ -60,20 +60,16 @@ do
             fi
             ;;
         --asan-ubsan)
-	    export ASAN_OPTIONS=handle_ioctl=1:detect_leaks=1:allow_user_segv_handler=1:use_sigaltstack=0:detect_deadlocks=1:intercept_tls_get_addr=1:check_initialization_order=1:detect_stack_use_after_return=1:strict_init_order=1:detect_invalid_pointer_pairs=1
-	    export LSAN_OPTIONS=print_suppressions=0:report_objects=1
-	    export UBSAN_OPTIONS=print_stacktrace=1
-	    export CC="clang -fno-sanitize-recover=undefined,integer -fsanitize=address -fsanitize=undefined -fsanitize=local-bounds"
-	    export CXX="clang++ -fno-sanitize-recover=undefined,integer -fsanitize-recover=float-divide-by-zero -fsanitize=address -fsanitize=undefined -fsanitize=local-bounds"
+	    export ASAN_OPTIONS=detect_stack_use_after_return=1
+	    export CC="clang -fsanitize=address -fsanitize=undefined"
+	    export CXX="clang++ -fsanitize=address -fsanitize=undefined"
 	    export CCACHE_CPP2=YES
             cmake_args+=" -DODFSIG_INTERNAL_XMLSEC=ON"
             ;;
         --fuzz)
-	    export ASAN_OPTIONS=handle_ioctl=1:detect_leaks=1:allow_user_segv_handler=1:use_sigaltstack=0:detect_deadlocks=1:intercept_tls_get_addr=1:check_initialization_order=1:detect_stack_use_after_return=1:strict_init_order=1:detect_invalid_pointer_pairs=1
-	    export LSAN_OPTIONS=print_suppressions=0:report_objects=1
-	    export UBSAN_OPTIONS=print_stacktrace=1
-	    export CC="$HOME/git/llvm/instdir/bin/clang -fno-sanitize-recover=undefined,integer -fsanitize=address -fsanitize=undefined -fsanitize=local-bounds -fsanitize=fuzzer-no-link"
-	    export CXX="$HOME/git/llvm/instdir/bin/clang++ -fno-sanitize-recover=undefined,integer -fsanitize-recover=float-divide-by-zero -fsanitize=address -fsanitize=undefined -fsanitize=local-bounds -fsanitize=fuzzer-no-link"
+	    export ASAN_OPTIONS=detect_stack_use_after_return=1
+	    export CC="$HOME/git/llvm/instdir/bin/clang -fsanitize=address -fsanitize=undefined -fsanitize=fuzzer-no-link"
+	    export CXX="$HOME/git/llvm/instdir/bin/clang++ -fsanitize=address -fsanitize=undefined -fsanitize=fuzzer-no-link"
 	    export CCACHE_CPP2=YES
             cmake_args+=" -DODFSIG_INTERNAL_XMLSEC=ON -DODFSIG_INTERNAL_LIBXML2=ON -DODFSIG_FUZZ=ON"
             ;;
