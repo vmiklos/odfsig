2cebfdc945756f41a44fb5170ef77f95f3ecbe31
Miklos Vajna <vmiklos@vmiklos.hu>
2020-11-28 11:09:18 +0100

    CI: restore linux-clang-debug config
    Change-Id: Icaca082e28a1109466510dbc5e28ca6fee0acc1b

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -5,8 +5,14 @@
 
 # Baseline: Ubuntu 18.04 and macOS 10.13.
 
+if [ -n "${GITHUB_WORKFLOW}" -a "$(uname -s)" == "Linux" ]; then
+    sudo apt-get install clang-tidy
+fi
+
 if [ "$GITHUB_JOB" == "linux-gcc-release" ]; then
     CI_ARGS="--werror"
+elif [ "$GITHUB_JOB" == "linux-clang-debug" ]; then
+    CI_ARGS="--debug --werror --clang"
 fi
 
 scripts/build.sh "$@" $CI_ARGS --internal-libs
