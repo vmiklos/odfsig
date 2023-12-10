7e9babbf5f46f635e053039861e5f44ffe0c20bf
Miklos Vajna <vmiklos@collabora.com>
2023-12-10 14:56:57 +0100

    scripts, up.sh: split out config options to a separate config file

--- scripts/up.sh
+++ scripts/up.sh
@@ -4,11 +4,13 @@
 # SPDX-License-Identifier: MIT
 
 # Wrapper around build.sh, providing good settings for development.
+#
+# Symlink both this script and autogen.input to the root of the source tree before using it.
 
 #git pull -r
 export CC=gcc-12
 export CXX=g++-12
-scripts/build.sh -DCMAKE_BUILD_TYPE=Debug -DODFSIG_ENABLE_WERROR=ON "$@"
+scripts/build.sh $(sed 's/#.*//' autogen.input) "$@"
 
 # Exclude workdir automatically.
 ctags --c++-kinds=+p --fields=+iaS --extra=+q -R --totals=yes $(git ls-files|grep /|sed 's|/.*||'|sort -u)
