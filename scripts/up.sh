eaf8293f1217c2c8f53195a337521e53fd346398
Miklos Vajna <vmiklos@collabora.com>
2023-12-17 16:02:03 +0100

    scripts: document opensuse container dependencies

--- scripts/up.sh
+++ scripts/up.sh
@@ -7,6 +7,22 @@
 #
 # Symlink both this script and autogen.input to the root of the source tree before using it.
 
+# Dependencies in a opensuse/leap:15.5 container (both system and internal libs config works after
+# this):
+# zypper in ccache
+# zypper in cmake
+# zypper in ctags
+# zypper in gcc12-c++
+# zypper in git
+# zypper in gtest
+# zypper in libtool
+# zypper in libxml2-devel
+# zypper in libzip-devel
+# zypper in mozilla-nss-devel
+# zypper in python-base
+# zypper in xmlsec1-nss-devel
+# zypper in zlib-devel
+
 #git pull -r
 export CC=gcc-12
 export CXX=g++-12
